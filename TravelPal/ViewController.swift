4d2b4f53e40b2ddb26a32191d56888c17b1cc743
Li Pengze <vonei@126.com>
2017-04-24 11:24:58 +0800

    Initial Commit

---
+++ TravelPal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelPal
+//
+//  Created by 李鹏泽 on 2017/4/24.
+//  Copyright © 2017年 INGSwifters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
